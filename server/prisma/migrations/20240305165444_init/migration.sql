123f8d72a0e6c1ec80ebf4c69def72f51342a2b3
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-03-05 13:04:43 -0500

    configure prisma

---
+++ server/prisma/migrations/20240305165444_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "contacts" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "phoneNumber" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
+);
