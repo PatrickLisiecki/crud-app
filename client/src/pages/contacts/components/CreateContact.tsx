59abb1d5ce35cff77447ffb8e35a2575af89ed02
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-03-05 20:24:17 -0500

    add toggle form button

--- client/src/pages/contacts/components/CreateContact.tsx
+++ client/src/pages/contacts/components/CreateContact.tsx
@@ -5,13 +5,18 @@ import { createContact } from "../api/createContact";
 
 import { ContactContext } from "@/contexts/ContactContext";
 
-export const CreateContact = () => {
-  const { setContacts } = useContext(ContactContext);
+interface CreateContactProps {
+  isOpen: boolean;
+  toggleIsOpen: Function;
+}
+
+export const CreateContact = ({ isOpen, toggleIsOpen }: CreateContactProps) => {
+  const { contacts, setContacts } = useContext(ContactContext);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [phone, setPhone] = useState<string>("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     const data = {
@@ -22,52 +27,71 @@ export const CreateContact = () => {
 
     const newContact = await createContact(data);
 
-    setContacts((prevContacts) => [...prevContacts, newContact]);
+    const newContacts = contacts.slice();
+    newContacts.push(newContact);
+    setContacts(newContacts);
+    toggleIsOpen();
   };
 
   return (
-    <div className="w-[350px] rounded-lg bg-gray-200 p-4 md:w-[500px]">
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center gap-y-2 p-4"
-      >
-        {/* Name Input */}
-        <div className="relative flex min-h-[40px] w-full flex-col justify-center">
-          <input
-            type="text"
-            value={name}
-            placeholder="John Smith"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
-          ></input>
-        </div>
-        {/* Email Input */}
-        <div className="relative flex min-h-[40px] w-full flex-col justify-center">
-          <input
-            type="text"
-            value={email}
-            placeholder="example@gmail.com"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
-          ></input>
-        </div>
-        {/* Phone Number Input */}
-        <div className="relative flex min-h-[40px] w-full flex-col justify-center">
-          <input
-            type="text"
-            value={phone}
-            placeholder="777-777-7777"
-            id="phone"
-            onChange={(e) => setPhone(e.target.value)}
-            className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
-          ></input>
-        </div>
-        <Button type="submit" size="lg">
-          Submit
-        </Button>
-      </form>
+    <div
+      className={`${isOpen ? "block" : "hidden"} fixed inset-0 z-50 flex items-center justify-center overflow-y-auto bg-black bg-opacity-50`}
+    >
+      <div className="mx-4 w-full rounded-lg bg-white p-8 md:max-w-md">
+        <h2 className="text-xl font-semibold">Create a Contact</h2>
+
+        <form
+          onSubmit={handleSubmit}
+          className="mt-4 flex flex-col items-center gap-y-2"
+        >
+          {/* Name Input */}
+          <div className="relative flex min-h-[40px] w-full flex-col justify-center">
+            <input
+              type="text"
+              value={name}
+              placeholder="John Smith"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
+            />
+          </div>
+          {/* Email Input */}
+          <div className="relative flex min-h-[40px] w-full flex-col justify-center">
+            <input
+              type="text"
+              value={email}
+              placeholder="example@gmail.com"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
+            />
+          </div>
+          {/* Phone Number Input */}
+          <div className="relative flex min-h-[40px] w-full flex-col justify-center">
+            <input
+              type="text"
+              value={phone}
+              placeholder="777-777-7777"
+              id="phone"
+              onChange={(e) => setPhone(e.target.value)}
+              className="text-dark placeholder:text-place dark:bg-dark block w-full cursor-text rounded border border-gray-500 p-4 focus:outline-none dark:text-white"
+            />
+          </div>
+          <div className="mt-4 flex w-full flex-row justify-evenly">
+            <Button
+              type="button"
+              variant="destructive"
+              size="lg"
+              onClick={() => toggleIsOpen()}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" size="lg">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
