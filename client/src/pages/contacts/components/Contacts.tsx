8801a46dc4c7fb94d926ee05d4c7ca118f022d59
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-03-05 20:23:50 -0500

    create toggle for create contact form

--- client/src/pages/contacts/components/Contacts.tsx
+++ client/src/pages/contacts/components/Contacts.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 import { CreateContact } from "./CreateContact";
 import { ContactList } from "./ContactList";
 
 export const Contacts = () => {
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+
+  const toggleIsCreating = () => {
+    setIsCreating(!isCreating);
+  };
+
   return (
     <div className="flex h-screen w-full flex-col bg-gray-100 px-24 py-12">
-      <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">Contacts</h1>
-      <CreateContact />
+      <div className="flex flex-row items-center gap-x-4">
+        <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">Contacts</h1>
+        <Button onClick={() => toggleIsCreating()} variant="default">
+          Create <Plus className="ml-2 h-4 w-4" />
+        </Button>
+        <Link to="/">
+          <Button variant="default">Home</Button>
+        </Link>
+      </div>
+      <CreateContact isOpen={isCreating} toggleIsOpen={toggleIsCreating} />
       <ContactList />
     </div>
   );
